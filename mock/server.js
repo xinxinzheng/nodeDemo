4e177418e7bcc11c8d4e68656c4b4b0ef7256298
Zheng <Winnie.Zheng@lionbridge.com>
2018-05-30 17:31:55 +0800

    all

--- mock/server.js
+++ mock/server.js
@@ -1,22 +1,18 @@
-var http = require('http');
+var express = require('express');
 var url=require('url');
 var Mock = require('mockjs');
-http.createServer(function (request, response) {
-    var pathname = url.parse(request.url).pathname;
+var app = express(); 
 
+app.all('*', function(req, res) {
     var data = Mock.mock({
         'list|1-10': [{
             // 属性 id 是一个自增数，起始值为 1，每次增 1
             'id|+1': 1
         }]
     });
-
-    response.writeHead(200, {'Content-type' : 'application/json; charset=utf-8'});
-    response.write(data);
-    response.end();
-    // switch(url){
-    //     case "/":
-    //         console.log(response)
-
-    // }
-}).listen(8082);
+    res.send(data);
+});
+/**
+ * 监听8082端口
+ */
+app.listen('8082');
